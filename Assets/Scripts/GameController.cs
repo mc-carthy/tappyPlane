a55b5e45fe1b35dc673bb5f46d0932fafbf8acdc
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-28 21:27:54 +0100

    Spawning obstacles

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,10 +9,34 @@ public class GameController : MonoBehaviour {
 	/// </summary>
 	public static float speedModifier;
 
+	[Header("Obstacle Information")]
+
+	[Tooltip("The obstacle that we will spawn")]
+	public GameObject obstacleReference;
+
+	[Tooltip("Minimum Y value used for obstacle")]
+	public float obstacleMinY = -1.3f;
+
+	[Tooltip("Maximum Y value used for obstacle")]
+	public float obstacleMaxY = 1.3f;
+
 	// Use this for initialization
 	void Start ()
 	{
 		speedModifier = 1.0f;
+		InvokeRepeating ("CreateObstacle", 1.5f, 1.0f);
+	}
+
+	/// <summary>
+	/// Creates the obstacle an initializes its position.
+	/// </summary>
+	void CreateObstacle()
+	{
+		// Spawn offscreen with a random Y
+		Instantiate(obstacleReference,
+			new Vector3(RepeatingBackground.scrollWidth, 
+				Random.Range(obstacleMinY, obstacleMaxY), 0.0f), 
+			Quaternion.identity);
 	}
 
 }
