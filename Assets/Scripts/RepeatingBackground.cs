69a4b9b76d246bc7a954645010c3e9a8ac799cd0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-06-28 18:38:01 +0100

    Add scrolling background

---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+
+	[Tooltip("How fast should the object move?")]
+	public float scrollSpeed;
+
+	/// <summary>
+	/// How far to move until the image is off-screen.
+	/// </summary>
+	public const float scrollWidth = 8; // bgWidth / pixelsPerUnit
+
+	private void FixedUpdate() {
+		Vector3 pos = transform.position;
+
+		// Move the object to the left
+		pos.x -= scrollSpeed * Time.fixedDeltaTime;
+
+		// Check if the object is off-screen
+		if (transform.position.x < -scrollWidth) {
+			Offscreen (ref pos);
+		}
+
+		// If not destroyed, set new position
+		transform.position = pos;
+	}
+
+	/// <summary>
+	/// Called when the object this is attached to is completely off-screen
+	/// </summary>
+	protected virtual void Offscreen(ref Vector3 pos) {
+		// Move object to be off-screen on the right
+		pos.x += 2 * scrollWidth;
+	}
+}
